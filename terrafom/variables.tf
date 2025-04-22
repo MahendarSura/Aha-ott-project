a99d03159d2d21df6062afba2fd65b713a8c3f00
Mahendra <mahendar.sura8@gmail.com>
2025-04-22 17:34:29 +0530

    Update variables.tf

--- terrafom/variables.tf
+++ terrafom/variables.tf
@@ -1,1 +1,26 @@
+# Variable for instance name
+variable "instance_name" {
+  description = "Name of the EC2 instance"
+  default     = "AHA_OTT_Instance"  # Default name of the instance
+}
+
+# Variable for AWS key pair name (for SSH access to EC2)
+variable "key_name" {
+  description = "Name of the SSH key pair"
+  default     = "starbucks1"  # Replace with your key pair name in AWS
+}
+
+# Variable for AWS access key
+variable "access_key" {
+  description = "AWS Access Key"
+  type        = string
+  default     = "insert"  # Replace with your AWS access key
+}
+
+# Variable for AWS secret key
+variable "secret_key" {
+  description = "AWS Secret Key"
+  type        = string
+  default     = "insert"  # Replace with your AWS secret key
+}
 
