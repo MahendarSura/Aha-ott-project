c2a5a8a279f7131cd04a007041975c8802e667cc
Mahendra <mahendar.sura8@gmail.com>
2025-04-22 17:28:17 +0530

    Update reportWebVitals.js

--- src/pages/reportWebVitals.js
+++ src/pages/reportWebVitals.js
@@ -1,1 +1,16 @@
+const reportWebVitals = (onPerfEntry) => {
+  if (onPerfEntry && onPerfEntry instanceof Function) {
+    // Dynamically import the web-vitals library
+    import('web-vitals').then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
+      // Report the different performance metrics
+      getCLS(onPerfEntry);  // Cumulative Layout Shift
+      getFID(onPerfEntry);  // First Input Delay
+      getFCP(onPerfEntry);  // First Contentful Paint
+      getLCP(onPerfEntry);  // Largest Contentful Paint
+      getTTFB(onPerfEntry); // Time to First Byte
+    });
+  }
+};
+
+export default reportWebVitals;
 
