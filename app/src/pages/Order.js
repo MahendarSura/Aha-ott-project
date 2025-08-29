11d2507b8a3c9cdb74ba3d4b8762db22f8bcfa2b
Mahendar Sura <mahendar.sura8@gmail.com>
2025-08-29 15:04:47 +0530

    Update Order.js

--- app/src/pages/Order.js
+++ app/src/pages/Order.js
@@ -1,1 +1,13 @@
 
+import React from 'react';
+
+const Order = () => {
+  return (
+    <div className='mahi1155 md:h-[400px] md:text-2xl flex items-center justify-center'>
+      Order
+    </div>
+  );
+};
+
+export default Order;
+
