b746e4c4bbeb8926f660d7b8afd5a036a99ca286
Mahendar Sura <mahendar.sura8@gmail.com>
2025-08-29 15:05:44 +0530

    Update Pay.js

--- app/src/pages/Pay.js
+++ app/src/pages/Pay.js
@@ -1,1 +1,13 @@
 
+import React from 'react';
+
+const Pay = () => {
+  return (
+    <div className='mahi1155 md:h-[400px] md:text-2xl flex items-center justify-center'>
+      Pay
+    </div>
+  );
+};
+
+export default Pay;
+
